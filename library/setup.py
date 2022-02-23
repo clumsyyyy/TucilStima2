f1fdc1e2e0a5ab102bffa1be36acab9581806915
clumsyyyy <owencwijaya@gmail.com>
2022-02-23 17:03:10 +0700

    renewed distance algo, updated library

---
+++ library/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "myConvexHull" ,
+    packages = find_packages(include = ['myConvexHull']),
+    version = '0.0.1',
+    description = 'Library to find the convex hull from points from a 2D plane',
+    author = 'Owen Christian Wijaya',
+    author_email = '13520124@std.stei.itb.ac.id',
+)
