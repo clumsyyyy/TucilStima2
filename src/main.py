83475970343ed6c252548c1d6317afa3db839ff5
clumsyyyy <owencwijaya@gmail.com>
2022-02-24 20:38:34 +0700

    update colors

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from myConvexHull.process import Convex
 from scipy.spatial import ConvexHull
 
+#%%
 plt.figure(figsize = (10, 6))
 plt.title('myConvexHull')
 data = datasets.load_iris()
@@ -21,18 +22,29 @@ colors = ['b', 'r', 'g']
 for i in range(len(data.target_names)):
     bucket = df[df['Target'] == i].iloc[:, [0, 1]].values
     hull = ConvexObj.ConvexHull(bucket)
-    plt.scatter(bucket[:, 0], bucket[:, 1], label=data.target_names[i])
+    plt.scatter(bucket[:, 0], bucket[:, 1], label=data.target_names[i], color = colors[i % len(colors)])
     plt.plot(hull[0], hull[1], color = colors[i % len(colors)])
 plt.legend()
 
 
+#%%
 plt.figure(figsize = (10, 6))
 plt.title('SciPy')
-for i in range(len(data.target_names)):
+datas = datasets.load_iris()
+dfs = pd.DataFrame(datas.data, columns = datas.feature_names)
+dfs['Target'] = pd.DataFrame(datas.target)
+
+plt.xlabel(datas.feature_names[0])
+plt.ylabel(datas.feature_names[1])
+
+scolors = ['c', 'm', 'y']
+
+for i in range(len(datas.target_names)):
     bucket = df[df['Target'] == i].iloc[:, [0, 1]].values
     hull = ConvexHull(bucket)
-    plt.scatter(bucket[:, 0], bucket[:, 1], label=data.target_names[i])
+    plt.scatter(bucket[:, 0], bucket[:, 1], label=datas.target_names[i], color = scolors[i % len(scolors)])
     for simplex in hull.simplices:
-        plt.plot(bucket[simplex, 0], bucket[simplex, 1], colors[i % len(colors)])
+        plt.plot(bucket[simplex, 0], bucket[simplex, 1], color = scolors[i % len(scolors)])
 plt.legend()
+
 # %%
