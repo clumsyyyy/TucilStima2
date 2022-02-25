5d8dc1ed0be271c7798e60961951440a3dd1c049
clumsyyyy <owencwijaya@gmail.com>
2022-02-25 15:24:06 +0700

    updated colors

--- src/main.py
+++ src/main.py
@@ -9,11 +9,11 @@ from scipy.spatial import ConvexHull
 
 option = 1
 while (option != 0):
-    print("\n\nChoose dataset: ")
+    print("\n\nChoose your dataset: ")
     print("1. Iris dataset")
     print("2. Wine dataset")
     print("3. Digits dataset")
-    print("4. Wine Quality dataset")
+    print("4. Breast cancer dataset")
     print("0. Exit")
     option = int(input("Input your option >>> "))
     
@@ -30,15 +30,19 @@ while (option != 0):
     elif option == 4:
         data = datasets.load_breast_cancer()
     elif option == 0:
+        print("Exiting program...")
         break
-    
+    print(" ")
     df = pd.DataFrame(data.data, columns = data.feature_names)
     df['Target'] = pd.DataFrame(data.target)
-    df.head()
+    print("Available columns:")
+    for i in range(len(df.columns) - 1):
+        print("{}. {}".format(i, df.columns[i]))
     
     print("\nInput your column (max:{}): ".format(len(df.columns)))
+    print("Note: the y-column taken would be the column right next to the inputted column!")
     cols = int(input("Input your column (max: {}):".format(len(df.columns))))
-    while (cols + 1 >= len(df.columns)):
+    while (cols + 1 >= len(df.columns) - 1):
         print("Invalid input! Try again.\n")
         cols = int(input("Input your column (max: {}):".format(len(df.columns))))
         
@@ -51,7 +55,7 @@ while (option != 0):
     plt.ylabel(data.feature_names[cols + 1])
 
     ConvexObj = Convex()
-    colors = ['b', 'r', 'g', 'y', 'm', 'c']
+    colors = ['b', 'r', 'g']
 
     for i in range(len(data.target_names)):
         bucket = df[df['Target'] == i].iloc[:, [cols, cols + 1]].values
@@ -73,15 +77,15 @@ while (option != 0):
     plt.xlabel(data.feature_names[cols])
     plt.ylabel(data.feature_names[cols + 1])
 
-
+    scolors = ["c", "m", "y"]
     for i in range(len(data.target_names)):
         bucket = df[df['Target'] == i].iloc[:, [cols, cols + 1]].values
         hull = ConvexHull(bucket)
         
-        if (i >= len(colors)):
+        if (i >= len(scolors)):
             tempColor = random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)
         else:
-            tempColor = colors[len(colors) - 1 - i]
+            tempColor = scolors[len(scolors) - 1 - i]
             
         plt.scatter(bucket[:, 0], bucket[:, 1], label=data.target_names[i], color = tempColor)
         for simplex in hull.simplices:
